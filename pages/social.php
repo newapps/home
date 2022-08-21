eca78aff411d1b02a512523ec10be4f616d4ed56
Ibrahim Qadir <65823225+tech-0@users.noreply.github.com>
2022-08-22 02:39:58 +0300

    Add files via upload

---
+++ pages/social.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui, viewport-fit=cover">
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'><link rel="stylesheet" href="./style.css">
+
+  <title>social media</title> 
+  <style>
+body {
+  margin:0;
+  padding:0;
+  background: #dedede;
+}
+
+ul {
+  position:absolute;
+  top:60%;
+  left:50%;
+  transform: translate(-50%, -50%);
+  margin:0;
+  padding:0;
+  display:flex;
+}
+
+ul li {
+  list-style: none;
+}
+
+ul li a {
+  position: relative;
+  width:50px;
+  height:50px;
+  display:block;
+  text-align:center;
+  margin:0 10px;
+  border-radius: 50%;
+  padding: 2px;
+  box-sizing: border-box;
+  text-decoration:none;
+  box-shadow: 0 10px 15px rgba(0,0,0,0.3);
+  background: linear-gradient(0deg, #ddd, #fff);
+  transition: .5s;
+}
+
+ul li a:hover {
+  box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+  text-decoration:none;
+}
+
+ul li a .fab {
+  widht: 60%;
+  height:60%;
+  display:block;
+  background: linear-gradient(0deg, #fff, #ddd);
+  border-radius: 50%;
+  line-height: calc(60px - 12px);
+  font-size:24px;
+  color: #262626;
+  transition: .5s;
+}
+
+ul li:nth-child(1) a:hover .fab {
+  color: #3b5998;
+}
+
+ul li:nth-child(2) a:hover .fab {
+  color: #00aced;
+}
+
+ul li:nth-child(3) a:hover .fab {
+  color: #dd4b39;
+}
+
+ul li:nth-child(4) a:hover .fab {
+  color: #007bb6;
+}
+
+ul li:nth-child(5) a:hover .fab {
+  color: #e4405f;
+}
+p{
+	font-size: 20;
+	color: white;
+	text-align: center;
+	margin-top: 50%;
+	padding: 5px;
+	margin-right: 10%;
+	margin-left: 10%;
+	background: gray;
+	border-radius: 25px;
+	 
+
+}
+#b{
+	background: clear;
+	border-radius: 15px;
+	font-size: 20px;
+	margin-top: 5px;
+	border-color: red;
+}
+</style>
+
+</head>
+
+<body>
+
+<p> لە رێگای ئەم تۆرە کۆمەڵایەتیانە لەگەلم بن </p>
+<br>
+<center>
+
+<input type="button" onclick="location.href='https://newapps.github.io/home/';" value="گەرانەوە" id="b"/>
+</center>
+
+<ul>
+  <li><a href="https://www.facebook.com/ibrahimmm55"><i class="fab fa-facebook"></i></a></li>
+   <li><a href="https://instagram.com/ittz._ibo"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+  <li><a href="https://is.gd/oOa7HO"><i class="fab fa-snapchat-ghost" aria-hidden="true"></i></a></li>
+  <li><a href="https://t.me/ibrahimiphone"><i class="fab fa-telegram" aria-hidden="true"></i></a></li>
+  <li><a href="https://twitter.com/ibrahemqadir1"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+  <li><a href="https://is.gd/Czd7kK"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
+ 
+</ul>
+  
+</body>
+</html>
